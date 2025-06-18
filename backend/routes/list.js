41e6738944885b0dcb4ed35a6a224402801ba03a
AdilAhmedShekhani <adilaziz2009@live.com>
2025-06-19 02:03:10 +0500

    Get Tasks

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -40,7 +40,7 @@ router.put("/updateTask/:id", async (req, res) => {
 router.delete("/deleteTask/:id", async (req, res) => {
     try {
         const { email } = req.body;
-        const existingUser = await User.findOne({ email })
+        const existingUser = await User.findOneAndUpdate({ email }, { $pull: { list: req.params.id } })
         if (existingUser) {
             const list = await List.findByIdAndDelete(req.params.id)
                 .then(() => res.status(200).json({ message: "Task Deleted" }))
@@ -49,6 +49,12 @@ router.delete("/deleteTask/:id", async (req, res) => {
 
     }
 });
+
+// Get Tasks
+router.get("/getTask/:id", async (req, res) => {
+    const list = await List.find({ user: req.params.id })
+    res.status(200).json({ list: list })
+})
 
 
 module.exports = router;
