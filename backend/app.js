ba994acc338d2c994a8625b43fa92ffa2179394d
AdilAhmedShekhani <adilaziz2009@live.com>
2025-06-19 01:26:57 +0500

    Task Created

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-
 require("./conn/conn");
-const auth = require("./routes/auth")
+
+const auth = require("./routes/auth");
+const list = require("./routes/list");
 app.use(express.json());
 
 
@@ -11,7 +12,9 @@ app.get("/", (req, res) => {
 });
 
 
-app.use("/api/v1", auth)
+app.use("/api/v1", auth);
+app.use("/api/v2", list);
+
 app.listen(1000, () => {
     console.log("Server Started");
 });
